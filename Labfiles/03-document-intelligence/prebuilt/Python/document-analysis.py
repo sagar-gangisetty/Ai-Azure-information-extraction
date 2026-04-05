28a57b97212b83360ce2f604ae435972c69b55b6
sagar-gangisetty <sagar.gangisetty@outlook.com>
2026-04-05 12:14:04 +0000

    Enhance document analysis and card processing with API integration and response handling

--- Labfiles/03-document-intelligence/prebuilt/Python/document-analysis.py
+++ Labfiles/03-document-intelligence/prebuilt/Python/document-analysis.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 import os
 
 # Add references
+# Add references
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
 
 
 
@@ -27,15 +30,29 @@ def main():
 
 
         # Create the client
-
-
+        document_analysis_client = DocumentAnalysisClient(
+            endpoint=endpoint, credential=AzureKeyCredential(key)
+        )
 
         # Analyse the invoice
-
-
+        poller = document_analysis_client.begin_analyze_document_from_url(
+            fileModelId, fileUri, locale=fileLocale)
 
         # Display invoice information to the user
+        receipts = poller.result()
 
+        for idx, receipt in enumerate(receipts.documents):
+            vendor_name = receipt.fields.get("VendorName")
+            if vendor_name:
+                print(f"\nVendor Name: {vendor_name.value}, with confidence {vendor_name.confidence}.")
+
+            customer_name = receipt.fields.get("CustomerName")
+            if customer_name:
+                print(f"Customer Name: {customer_name.value}, with confidence {customer_name.confidence}.")
+
+            invoice_total = receipt.fields.get("InvoiceTotal")
+            if invoice_total:
+                print(f"Invoice Total: {invoice_total.value}, with confidence {invoice_total.confidence}.")
 
             
 
